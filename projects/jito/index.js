d421ba41f8f1c8ef5650d978f59777dffda5d339
Push <pushpushX@protonmail.com>
2025-03-17 23:11:01 +1100

    Remove Jito base tvl and leave as placeholder

--- projects/jito/index.js
+++ projects/jito/index.js
@@ -1,20 +1,17 @@
-const { PublicKey } = require("@solana/web3.js");
-const { getConnection } = require("../helper/solana");
-
 async function tvl() {
-  // https://jito.network/staking
-  const connection = getConnection();
-  const account = await connection.getAccountInfo(new PublicKey('Jito4APyf642JPZPx3hGc6WWJ8zPKtRbRs4P815Awbb'))
   return {
-    solana: Number(account.data.readBigUint64LE(258))/1e9
+    solana: 0
   }
 }
 
 module.exports = {
   timetravel: false,
   methodology:
-    "Uses the SPL Stake Pool SDK to fetch the total supply of deposited SOL into the Jito Stake Pool",
+    "Placeholder",
   solana: {
     tvl,
   },
+  hallmarks: [
+    [1706490000,"launch jito exchange"]
+  ],
 };
